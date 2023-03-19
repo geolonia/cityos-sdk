70484c67f62e57a22c47f85b93757be2cf648179
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2023-03-20 02:48:47 +0900

    added loadCSV function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import Papa from 'papaparse';
 
 import style from './style.json'
 
@@ -42,6 +43,68 @@ class TakamatsuMap extends maplibregl.Map {
         ],
       ],
     }, 'poi');
+  }
+
+  async loadCSV(url: string) {
+    // Fetch the csv from the url
+    const res = await fetch(url);
+    const csv = await res.text();
+
+    const data = Papa.parse(csv, {header: true}).data
+
+    // Convert the data to geojson use `緯度` as latitude and `経度` as longitude
+    const geojson = {
+      type: 'FeatureCollection',
+      features: data.map((d: any) => ({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [d.経度, d.緯度]
+        },
+        properties: d
+      }))
+    }
+
+    this.addSource(url, {
+      type: 'geojson',
+      data: geojson,
+    })
+
+    // Add the geojson as layer to the map
+    this.addLayer({
+      id: url,
+      type: 'circle',
+      source: url,
+      paint: {
+        'circle-radius': 5,
+        'circle-color': '#FF0000',
+        'circle-opacity': 0.5,
+      }
+    }, 'poi');
+
+    this.addLayer(    {
+      "id": `symbol-${url}`,
+      "type": "symbol",
+      "source": url,
+      "layout": {
+        'text-field': "{名称}",
+        "text-font": [
+          "Noto Sans CJK JP Bold"
+        ],
+        'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+        'text-radial-offset': 0.5,
+        'text-justify': 'auto',
+        'text-size': 12,
+        'text-anchor': 'top',
+        'text-max-width': 12,
+        'text-allow-overlap': false,
+      },
+      "paint": {
+        "text-color": "#333",
+        "text-halo-width": 1.2,
+        "text-halo-color": "rgba(255,255,255,0.8)"
+      }
+    })
   }
 }
 
